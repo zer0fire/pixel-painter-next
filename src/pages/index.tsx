0e8f4eca41dac66a38d5892094a623f866b422ae
wangmeng <wangmeng08@sparkedu.com>
2023-10-20 15:55:07 +0800

    fix: 干掉字体

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 "use client";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import App from "./Home";
 import { useEffect } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const start = async () => {
